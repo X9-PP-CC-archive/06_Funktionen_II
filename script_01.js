90a0456c51c89bd4e55da46054f2d43a570a30a1
Peter <schmidt.multimedia@gmail.com>
2023-04-05 11:08:32 +0200

    Grundrechenarten +|-|*|:

--- script_01.js
+++ script_01.js
@@ -7,11 +7,11 @@
 
 1. Dateneingabe + -überprüfung :
 2. Auswahl Rechenart : 
-3. Fkt. Grundrechenarten : 
-4. Ausgabe in Konsole : 
+3. Fkt. Grundrechenarten : check!
+4. Ausgabe in Konsole : check!
 */
 
-/
+
 
 
 // Konstanten
@@ -19,6 +19,52 @@ const ERROR_STR_DIV = "Division durch 0 nicht möglich!";
 const ERROR_STR_GEN = "Irgendetwas ging schief!"
 
 
+
+
+// module: division a / b |  test:
+// output(divide(4,2));
+// output(divide(3,2));
+// output(divide(3,-2));
+// output(divide(3,0));
+function divide(a,b) {
+
+	// if (b == 0)  {
+	// 	return ERROR_STR_DIV;
+	// } else {
+	// 	return a / b;
+	// }
+
+	if (b == 0)  {
+		return ERROR_STR_DIV;
+	} 
+	
+	return a / b;
+	
+}
+
+// module: multiplication a * b |  test:
+// output(multiply(3,2));
+// output(multiply(3,-2));
+// output(multiply(3,0));
+function multiply(a,b) {
+	return a * b;
+}
+
+// module: subtraction a - b |  test:
+// output(subtract(3,2));
+// output(subtract(3,-2));
+// output(subtract(3,0));
+function subtract(a,b) {
+	return a - b;
+}
+
+// module: addition a + b |  test:
+// output(add(2,2));
+// output(add(2,-2));
+// output(add(2,0));
+function add(a,b) {
+	return a + b;
+}
 
 // module: output | test:
 // output("hello");
