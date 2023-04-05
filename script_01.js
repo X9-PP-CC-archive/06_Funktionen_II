fc54fad7f7d3198205757e3c180ef026131a07d0
Peter Schmidt <schmidt.multimedia@gmail.com>
2023-04-05 10:18:45 +0200

    Add files via upload

---
+++ script_01.js
@@ -0,0 +1,29 @@
+
+"use strict";
+
+/*** Rechner */
+/*
+0. a+b | a-b | a*b | a/b  --> Ergebnis c 
+
+1. Dateneingabe + -überprüfung :
+2. Auswahl Rechenart : 
+3. Fkt. Grundrechenarten : 
+4. Ausgabe in Konsole : 
+*/
+
+/
+
+
+// Konstanten
+const ERROR_STR_DIV = "Division durch 0 nicht möglich!";
+const ERROR_STR_GEN = "Irgendetwas ging schief!"
+
+
+
+// module: output | test:
+// output("hello");
+// output(2);
+function output(outputData) {
+	console.log(outputData);
+}
+  
